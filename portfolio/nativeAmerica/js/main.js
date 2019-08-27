3e9df2855cf37a2302bf947a8493b0444c318c30
Timothy Prestby <tjprestby@gmail.com>
2019-08-27 11:40:36 -0500

    Update main.js

--- portfolio/nativeAmerica/js/main.js
+++ portfolio/nativeAmerica/js/main.js
@@ -504,7 +504,7 @@ function updateSlider(timelineSlider){
 
 //Function: Load all the data using AJAX//
 function loadData(map, year){
-    $.ajax("./NativeLand1880On.geojson", {
+    $.ajax("./data/NativeLand1880On.geojson", {
         dataType: "json",
         success: function(response){
             // Sort the data in yearMap by year
@@ -517,7 +517,7 @@ function loadData(map, year){
             };
         }
     });
-    $.ajax("./NativeLandPre1880.geojson", {
+    $.ajax("./data/NativeLandPre1880.geojson", {
         dataType: "json",
         success: function(response){
             // Sort the data in yearMap by year
@@ -530,7 +530,7 @@ function loadData(map, year){
             };
         }
     });
-    $.ajax("./LandLostWithoutTreaty.geojson", {
+    $.ajax("./data/LandLostWithoutTreaty.geojson", {
         dataType: "json",
         success: function(response){
             // Sort the data in yearMap by year
