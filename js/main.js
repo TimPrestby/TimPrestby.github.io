4e30cc2fbb1b8830ff3908b61722ab9ea816af40
Timothy Prestby <tjprestby@gmail.com>
2019-11-22 17:27:49 -0600

    Updated Website

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  //Allow for smooth scrolling//
+  document.getElementById("intro").addEventListener("click", smoothScroll);
+  
+  function smoothScroll(){
+    document.querySelector('#intro').scrollIntoView({ 
+        behavior: 'smooth' 
+    })
+  }
+
+  //Determine the filename to trigger cover page
+  var url = window.location.pathname;
+  var filename = url.substring(url.lastIndexOf('/')+1);
+
+  if(filename == "index.html"){
+    //Create scroll monitor to change header//
+    var introWatcher = scrollMonitor.create($('.arrow'), 1);
+
+    introWatcher.exitViewport(function (){
+        $("nav").addClass('fixed');
+    });
+  } else{
+    $("nav").addClass('fixed');
+
+  }
+  //Animate skill chart
+
+});
+
